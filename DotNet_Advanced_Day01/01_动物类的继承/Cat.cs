1235eae68696275df2feea440ad69befe770ea98
Fowindy <47002339@qq.com>
2019-05-08 16:37:55 +0800

    01_动物类的继承_Cat子类

--- DotNet_Advanced_Day01/01_动物类的继承/Cat.cs
+++ DotNet_Advanced_Day01/01_动物类的继承/Cat.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace _01_动物类的继承
 {
-    public class Cat
+    //Cat子类继承Animal父类
+    public class Cat:Animal
     {
+        //子类必须实现(重写)父类中的抽象方法,
+        //除非子类也是抽象类
+        public override void Eat()
+        {
+            Console.WriteLine("猫咪舔着吃...");
+        }
+
+        public override void Shout()
+        {
+            Console.WriteLine("猫咪喵喵叫...");
+        }
     }
 }
