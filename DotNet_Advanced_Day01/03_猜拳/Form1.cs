a78b5505f65ed92a4b43b2ff0f978341b13a7775
Fowindy <47002339@qq.com>
2019-05-09 17:19:55 +0800

    03_猜拳_石头按钮完成_待封装

--- DotNet_Advanced_Day01/03_猜拳/Form1.cs
+++ DotNet_Advanced_Day01/03_猜拳/Form1.cs
@@ -16,5 +16,28 @@ namespace _03_猜拳
         {
             InitializeComponent();
         }
+
+        private void btnStone_Click(object sender, EventArgs e)
+        {
+            //点击按钮开始玩游戏
+            //1.首先创建一个玩家
+            Player player = new Player();
+            //2.玩家开始出拳
+            int playerNum = player.PlayerPlay(btnStone.Text);
+            //3.将玩家出拳的内容显示在lab控件上
+            labPlayerResult.Text = player.PlayerFistName;
+
+            //4.创建一个电脑
+            Computer computer = new Computer();
+            //5.电脑开始出拳
+            int computerNum = computer.ComputerPlay();
+            //5.将电脑出拳的内容显示在label控件上
+            labComputerResult.Text = computer.ComputerFistName;
+
+            //5.创建一个裁判
+            Judge judge = new Judge();
+            //6.将输赢的结果显示在label控件上
+            labWinResult.Text = judge.Win(playerNum,computerNum);
+        }
     }
 }
