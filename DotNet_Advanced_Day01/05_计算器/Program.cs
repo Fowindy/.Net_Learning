3a81234fc43431b9e46ab77e8fefb1d44ef24dd6
Fowindy <47002339@qq.com>
2019-05-11 07:52:01 +0800

    05_计算器_初步完成_后接错误说明1

---
+++ DotNet_Advanced_Day01/05_计算器/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_计算器
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //控制台形式的计算器
+            Console.Write("请输入第一个数:");
+            int num1 = Convert.ToInt32(Console.ReadLine());//可能会报异常
+            Console.Write("请输入第二个数:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("请输入一个操作符(+,-,*,/):");
+            string opt = Console.ReadLine();//符号
+
+
+            //类---类库的方式
+        }
+    }
+}
---
+++ DotNet_Advanced_Day01/Calculator/Add.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Add : JiSuan
+    {
+        public override int GetResult(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+        //public Add(int num1, int num2) : base(num1, num2)
+        //{
+        //}
+    }
+}
