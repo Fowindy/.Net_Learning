05765e1139e509b663e0f7ceffa09c7bf71b6ccf
Fowindy <47002339@qq.com>
2019-05-12 12:30:09 +0800

    1_多种参数的构造函数

--- DotNet_Advanced_Day02/01_This的一种用法/Program.cs
+++ DotNet_Advanced_Day02/01_This的一种用法/Program.cs
@@ -10,8 +10,31 @@ namespace _01_This的一种用法
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("DotNet_Advanced_Day02");
-            Console.ReadLine();
+        }
+    }
+
+    public class Person
+    {
+        //1_2-1.name自动属性
+        public string Name { get; set; }
+        //1_2-2.age自动属性
+        public int Age { get; set; }
+
+        //1_1.无参的构造函数
+        public Person()
+        {
+
+        }
+        //1_2.有一个参数的构造函数
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+        //1_3.两个参数的构造函数
+        public Person(string name,int age)
+        {
+            this.Name = name;
+            this.Age = age;
         }
     }
 }
